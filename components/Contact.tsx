80e69087bec07ecc4bee04654d1c9ed4ab17d6aa
Toxcastel <a.castelr@gmail.com>
2022-11-26 00:24:34 -0300

    contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import React from "react";
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
 
-type Props = {}
+type Props = {};
 
 const Contact = (props: Props) => {
-  return (
-    <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-        
-        <h3 className='md:absolute md:top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
-            
-            Contact
-            </h3>
-        </div>
-  )
-}
+    return (
+        <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+            <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Contact</h3>
 
-export default Contact
+            <div className="flex flex-col space-y-10">
+                <h4 className="text-3xl font-semibold text-center ">
+                    {`Would you like my help? Let's `}
+                    <span className=" text-[#F7AB0A]">Talk</span>
+                </h4>
+                <div className="space-y-10">
+                    <div className="flex items-center space-x-5 justify-center">
+                        <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+                        <p>+56945965201</p>
+                    </div>
+                    <div className="flex items-center space-x-5 justify-center">
+                        <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+                        <p>contact@albertocastel.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Contact;
