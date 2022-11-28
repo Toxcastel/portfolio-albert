3e4f3d2218cbe26ceec5e1e2617dcdead1caace7
Toxcastel <a.castelr@gmail.com>
2022-11-28 14:22:22 -0300

    Contact info

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -15,14 +15,20 @@ const Contact = (props: Props) => {
                 </h4>
                 <div className="space-y-10">
                     <div className="flex items-center space-x-5 justify-center">
-                        <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
-                        <p>+56945965201</p>
-                    </div>
-                    <div className="flex items-center space-x-5 justify-center">
                         <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
                         <p>contact@albertocastel.com</p>
                     </div>
                 </div>
+                <form className="flex flex-col space-y-2 w-fit mx-auto">
+                    <div>
+                        <input type="text" /><input type="text" />
+                    </div>
+
+                    <input type="text" />
+
+                    <textarea />
+                    <button>Submit</button>
+                </form>
             </div>
         </div>
     );
