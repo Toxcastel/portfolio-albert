1d3a56c686b63479d68fdba766c004bd6d7419eb
toxcastel <a.castelr@gmail.com>
2023-01-16 18:29:15 -0300

    Fixing titles in both mobile and desktop views

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -11,10 +11,10 @@ const Skills = (props: Props) => {
             transition={{ duration: 1.5 }}
             className="h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
         >
-            <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Skills</h3>
+            <h3 className="absolute top-0 p-5 sm:top-[4rem] uppercase sm:tracking-[20px] tracking-[10px]  text-gray-500 text-2xl">Skills</h3>
 
 
-            <div className="grid grid-cols-4 gap-5">
+            <div className="grid grid-cols-3 sm:grid-cols-4 gap-5">
                 <Skill />
                 <Skill />
                 <Skill />
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -12,9 +12,9 @@ const WorkExperience = ({ experiences }: Props) => {
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }}
-            className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+            className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto sm:items-center items-end"
         >
-            <h3 className="absolute top-[4rem] uppercase tracking-[20px] text-gray-500 text-2xl hidden md:block">Experience</h3>
+            <h3 className="absolute top-0 p-5 sm:top-[4rem] uppercase sm:tracking-[20px] tracking-[10px] text-gray-500 text-2xl ">Experience</h3>
             <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory h-[80%] scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
                 {experiences?.map((experience) => (
                     <ExperienceCard key={experience?._id} experience={experience} />
