6d46dd4937a6d7bd04782d1e600c1ae8f0308752
toxcastel <a.castelr@gmail.com>
2023-01-16 19:20:42 -0300

    fix in about

--- components/About.tsx
+++ components/About.tsx
@@ -13,7 +13,7 @@ function About({pageInfo}: Props) {
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }}
-            className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+            className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-center mx-auto items-center"
         >
             <h3 className="absolute top-0 p-5 sm:top-[4rem] uppercase sm:tracking-[20px] tracking-[10px] text-gray-500 text-2xl">About</h3>
 
@@ -35,7 +35,7 @@ function About({pageInfo}: Props) {
                     alt="stf-tato"
                     src={urlFor(pageInfo?.profilePic).url()}
                     width={1000}
-                    className="-mb-20 md:mb-0 flex-shrink-0 max-h-[400px] md:block hidden rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
+                    className="-mb-20 md:mb-0 flex-shrink-0 sm:max-h-[400px] sm:flex hidden rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
                     priority
                     height={1000}
                 />
