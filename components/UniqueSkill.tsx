51f23feade6db49d4d474d1e8b2f22e612e2fe38
toxcastel <a.castelr@gmail.com>
2023-01-29 23:20:33 -0300

    Test

--- components/UniqueSkill.tsx
+++ components/UniqueSkill.tsx
@@ -22,6 +22,7 @@ const UniqueSkill = ({ directionLeft, skill }: Props) => {
             />
             {/* <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
                 <motion.div
+                
                     initial={{
                         x: directionLeft ? -100 : 100,
                         opacity: 0,
